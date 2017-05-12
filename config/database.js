f890be63938fd1fb6f5cdf5fa1978f060456f2c1
Ben Adamski <benjadamski@gmail.com>
2017-05-12 09:28:02 -0400

    Replace all instances of project name
    -  All instances of express-api-template changed to express-deploy-me.
    -  This is a skeleton app for testing deployment methods.

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const mongooseBaseName = 'express-api-template'
+const mongooseBaseName = 'express-deploy-me'
 
 const environment = process.env.NODE_ENV || 'development'
 const database = {
